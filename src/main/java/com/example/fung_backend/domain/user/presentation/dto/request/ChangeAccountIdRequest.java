b70da534b2ef67855347417110c2e521421d81a9
sorim <enhld00@gmail.com>
2024-10-22 23:34:42 +0900

    ⚡️ :: 유저 관련 요청 추가

---
+++ src/main/java/com/example/fung_backend/domain/user/presentation/dto/request/ChangeAccountIdRequest.java
@@ -0,0 +1,10 @@
+package com.example.fung_backend.domain.user.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@NoArgsConstructor
+public class ChangeAccountIdRequest {
+
+    private String accountId;
+}
