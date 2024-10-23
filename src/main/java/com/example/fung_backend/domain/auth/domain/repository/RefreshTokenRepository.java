14270f9b7542b96777ba97ba0b6446a8372f587e
sorim <enhld00@gmail.com>
2024-10-23 22:29:56 +0900

    ⚡️ :: 리프레쉬 토큰 레포지토리 추가

---
+++ src/main/java/com/example/fung_backend/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.example.fung_backend.domain.auth.domain.repository;
+
+import com.example.fung_backend.domain.auth.domain.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
